01e575b133e01b4bafa76012021da62755349839
Tobias Foerg <tobias.foerg@gmail.com>
2023-10-14 11:35:52 +0200

    add compose refs package

---
+++ packages/compose-refs/src/compose-refs.ts
@@ -0,0 +1,22 @@
+import * as React from "react";
+
+type PossibleRef<T> = React.Ref<T> | undefined;
+
+function setRef<T>(ref: PossibleRef<T>, value: T) {
+	if (typeof ref === "function") {
+		ref(value);
+	} else if (ref !== null && ref !== undefined) {
+		(ref as React.MutableRefObject<T>).current = value;
+	}
+}
+
+function composeRefs<T>(...refs: PossibleRef<T>[]) {
+	return (node: T) => refs.forEach((ref) => setRef(ref, node));
+}
+
+function useComposedRefs<T>(...refs: PossibleRef<T>[]) {
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	return React.useCallback(composeRefs(...refs), refs);
+}
+
+export { composeRefs, useComposedRefs };
